5095238f4f23946dc872af23423ed958c76b8b00
spayder26 <spayder26@11e82032-b753-11de-8dfb-43c0dffa5306>
2009-10-12 18:05:37 +0000

    First upload, 0.1 branch (0.1-2 release)

---
+++ debian/pywinery.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pywinery initscript
+# sourced by /etc/init.d/pywinery
+# installed at /etc/default/pywinery by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
